b770770d2868e8340c9a12a7d299cf9140456849
TomProyectoX <franrodriguez@uade.edu.ar>
2026-05-20 02:24:53 -0300

    componente filter

--- frontend/client/src/pages/ProductList.jsx
+++ frontend/client/src/pages/ProductList.jsx
@@ -1,46 +1,9 @@
-import { useEffect, useState } from "react"
-import ProductoCard from "../assets/components/react/ProductoCard.jsx"
+import FilterCategories from "../assets/components/react/FilterCategories"
 
-const ProductList = () => {
-  const [productos, setProductos] = useState([])
-  // filtro seleccionado
-  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("")
-  const [price, setPrice] = useState(250);
-
-  const URL = "http://localhost:4002/productos"
+function ProductList() {
 
 
-export default function 
-  const getcategorias = async () => {
-const response = await fetch('http://localhost:4002/categories', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-const data = await response.json();
 
-const categories = data.map(categorie =>(
-   categorie.description
-))
-
-return categories
-
-}
-
-  // productos filtrados
-const productosFiltrados = categoriaSeleccionada
-  ? productos.filter(
-      (producto) =>
-        producto.categoria.description.toLowerCase() ===
-        categoriaSeleccionada.toLowerCase()
-    )
-  : productos
-
-  // Función para poder marcar y desmarcar la categoría
-  const handleCategoryChange = (categoria) => {
-    setCategoriaSeleccionada((prev) => (prev === categoria ? "" : categoria))
-  }
 
   return (
     <div className="bg-[#0A0A0A] text-white min-h-screen pt-24 px-6">
@@ -58,57 +21,10 @@ const productosFiltrados = categoriaSeleccionada
             </div>
 
             {/* CATEGORY */}
-            <div className="space-y-4">
-              <h3 className="text-xl font-black uppercase text-[#CCFF00]">
-                Category
-              </h3>
-
-              <div className="flex flex-col gap-2">
-                <label className="flex items-center gap-3 cursor-pointer group">
-                  <input 
-                    type="checkbox" 
-                    className="w-4 h-4 rounded-none border-[#262626] !bg-black checked:!bg-[#CCFF00] text-black focus:ring-0 cursor-pointer" 
-                  />
-                </label>
-
-                <label className="flex items-center gap-3 cursor-pointer group">
-                 
-                </label>
-
-                <label className="flex items-center gap-3 cursor-pointer group">
-               
-                </label>
-              </div>
-            </div>
+            <FilterCategories type={"categoria"}/>
 
             {/* BRAND */}
-            <div className="space-y-4">
-              <h3 className="text-xl font-black uppercase text-[#CCFF00]">
-                Brand
-              </h3>
-
-              <div className="flex flex-col gap-2">
-                <label className="flex items-center gap-3 cursor-pointer group">
-                  <input 
-                    type="checkbox" 
-                    className="w-4 h-4 rounded-none border-[#262626] !bg-black checked:!bg-[#CCFF00] text-black focus:ring-0 cursor-pointer" 
-                  />
-                  <span className="text-xs font-bold uppercase text-white group-hover:text-[#CCFF00] transition-colors">
-                    Iron Arch Elite
-                  </span>
-                </label>
-
-                <label className="flex items-center gap-3 cursor-pointer group">
-                  <input 
-                    type="checkbox" 
-                    className="w-4 h-4 rounded-none border-[#262626] !bg-black checked:!bg-[#CCFF00] text-black focus:ring-0 cursor-pointer" 
-                  />
-                  <span className="text-xs font-bold uppercase text-white group-hover:text-[#CCFF00] transition-colors">
-                    Raw Kinetic
-                  </span>
-                </label>
-              </div>
-            </div>
+             <FilterCategories type={"marca"}/>
 
             {/* FLAVOR */}
             <div className="space-y-4">
@@ -117,19 +33,10 @@ const productosFiltrados = categoriaSeleccionada
               </h3>
 
               <div className="flex flex-col gap-2">
-                {["Chocolate", "Vanilla", "Berry Blast", "Fruit Punch"].map(
-                  (flavor) => (
-                    <label key={flavor} className="flex items-center gap-3 cursor-pointer group">
-                      <input 
-                        type="checkbox" 
-                        className="w-4 h-4 rounded-none border-[#262626] !bg-black checked:!bg-[#CCFF00] text-black focus:ring-0 cursor-pointer" 
-                      />
-                      <span className="text-xs font-bold uppercase text-white group-hover:text-[#CCFF00] transition-colors">
-                        {flavor}
-                      </span>
-                    </label>
-                  )
-                )}
+                <span className="text-xs font-bold uppercase text-white">Chocolate</span>
+                <span className="text-xs font-bold uppercase text-white">Vanilla</span>
+                <span className="text-xs font-bold uppercase text-white">Berry Blast</span>
+                <span className="text-xs font-bold uppercase text-white">Fruit Punch</span>
               </div>
             </div>
 
@@ -141,20 +48,12 @@ const productosFiltrados = categoriaSeleccionada
       <h3 className="text-xl font-black uppercase text-[#CCFF00]">
         Price Range
       </h3>
-      {/* Acá mostramos el precio actual dinámicamente */}
       <span className="text-white bg-[#141414] border border-[#262626] px-3 py-1 font-bold text-sm tracking-wider">
-        ${price}
+        $250
       </span>
     </div>
 
-    <input
-      type="range"
-      min="0"
-      max="250"
-      value={price} // Vinculamos el valor de la barra al estado
-      onChange={(e) => setPrice(Number(e.target.value))} // Actualiza el estado al moverla
-      className="w-full h-2 bg-black appearance-none cursor-pointer accent-[#CCFF00]"
-    />
+    <div className="w-full h-2 bg-black border border-[#262626]" />
 
     <div className="flex justify-between text-sm text-[#c4c9ac] font-bold">
       <span>$0</span>
@@ -188,7 +87,7 @@ const productosFiltrados = categoriaSeleccionada
           <div className="flex justify-between items-end mb-8">
             <div>
               <p className="text-sm text-gray-400 uppercase mb-2">
-                Showing {productosFiltrados.length} Results
+                Showing 3 Results
               </p>
 
               <h1 className="text-5xl font-black uppercase italic">
@@ -197,12 +96,56 @@ const productosFiltrados = categoriaSeleccionada
             </div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {productosFiltrados.map((producto) => (
-              <ProductoCard
-                key={producto.idProducto}
-                producto={producto}
-              />
-            ))}
+            <div className="bg-[#141414] border border-[#262626]">
+              <div className="relative aspect-square overflow-hidden bg-[#1f1f1f]">
+                <img
+                  src="https://placehold.co/600x600"
+                  alt="Protein"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <div className="flex justify-between items-start mb-2">
+                  <h3 className="text-xl font-bold text-white uppercase">Protein</h3>
+                  <span className="text-[#CCFF00] text-2xl font-black">$59</span>
+                </div>
+                <p className="text-gray-400 text-sm mb-6">Basic product card.</p>
+              </div>
+            </div>
+
+            <div className="bg-[#141414] border border-[#262626]">
+              <div className="relative aspect-square overflow-hidden bg-[#1f1f1f]">
+                <img
+                  src="https://placehold.co/600x600"
+                  alt="Creatine"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <div className="flex justify-between items-start mb-2">
+                  <h3 className="text-xl font-bold text-white uppercase">Creatine</h3>
+                  <span className="text-[#CCFF00] text-2xl font-black">$39</span>
+                </div>
+                <p className="text-gray-400 text-sm mb-6">Basic product card.</p>
+              </div>
+            </div>
+
+            <div className="bg-[#141414] border border-[#262626]">
+              <div className="relative aspect-square overflow-hidden bg-[#1f1f1f]">
+                <img
+                  src="https://placehold.co/600x600"
+                  alt="Pre Workout"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <div className="flex justify-between items-start mb-2">
+                  <h3 className="text-xl font-bold text-white uppercase">Pre Workout</h3>
+                  <span className="text-[#CCFF00] text-2xl font-black">$45</span>
+                </div>
+                <p className="text-gray-400 text-sm mb-6">Basic product card.</p>
+              </div>
+            </div>
           </div>
         </section>
       </div>
